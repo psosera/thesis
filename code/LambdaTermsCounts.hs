712c8093b22f1abd2cf88752252ca00f43ffbb4b
Peter-Michael Osera <psosera@gmail.com>
2015-05-07 16:43:38 -0400

    Checkpoint---rev'ed the content of ch1 to include base types.

--- code/LambdaTermsCounts.hs
+++ code/LambdaTermsCounts.hs
@@ -1,8 +1,9 @@
+import Data.List
 import Data.Maybe
 import Text.Printf
 
 data Type
-  = TUnit
+  = TBase
   | TArr Type Type
   deriving (Show, Eq)
 
@@ -10,17 +11,20 @@ data Exp
   = EVar String
   | EApp Exp Exp
   | EAbs String Type Exp
-  | EUnit
+  | EConst Int
   deriving (Show, Eq)
 
 type Ctx = [(String, Type)]
+
+numOfConsts :: Int
+numOfConsts = 2
 
 normal :: Exp -> Bool
 normal (EVar x)               = True
 normal (EApp (EAbs _ _ _) e2) = False
 normal (EApp _            e2) = normal e2
 normal (EAbs _ _ e)           = normal e
-normal EUnit                  = True
+normal (EConst _)             = True
 
 freshName :: String -> Ctx -> String
 freshName prefix g = fresh 0
@@ -41,14 +45,14 @@ partitionPairs n = count 1
 
 ugenTyp :: Int -> [Type]
 ugenTyp 0 = []
-ugenTyp 1 = [TUnit]
+ugenTyp 1 = [TBase]
 ugenTyp n = concatMap genArr $ partitionPairs n
   where
     genArr (n1, n2) = [TArr t1 t2 | t1 <- ugenTyp n1, t2 <- ugenTyp n2]
 
 ugen :: Ctx -> Int -> [Exp]
 ugen g 0 = []
-ugen g 1 = EUnit : map (EVar . fst) g
+ugen g 1 = map EConst [1..numOfConsts] ++ map (EVar . fst) g
 ugen g n = concatMap genAbs sizes ++ concatMap genApp sizes
   where
     sizes           = partitionPairs (n-1)
@@ -69,7 +73,7 @@ tc g (EApp e1 e2)  = do
 tc g (EAbs x t1 e) = do
   t2 <- tc ((x,t1):g) e
   return $ TArr t1 t2
-tc g EUnit         = return TUnit
+tc g (EConst _)    = return TBase
 
 welltyped :: Ctx -> Exp -> Bool
 welltyped g e = isJust $ tc g e
@@ -80,21 +84,28 @@ welltypedAt g t e =
     Just t' -> t == t'
     Nothing -> False
 
-calculateCounts :: Ctx -> Type -> Int -> (Int, Int, Int, Int, Int)
+calculateCounts :: Ctx -> Type -> Int -> [(Int, Int, Int, Int, Int)]
 calculateCounts g t n =
-  (n, rawCount, typedCount, typedAtCount, normalCount)
+  zip5 indices rawCounts typedCounts typedAtCounts normalCounts
   where
-    terms        = ugen g n
-    rawCount     = length terms
-    typedCount   = length $ filter (welltyped g) terms
-    typedAtCount = length $ filter (welltypedAt g t) terms
-    normalCount  = length $ filter normal $ filter (welltypedAt g t) terms
+    aggregate :: [Int] -> [Int]
+    aggregate xs = reverse res
+      where
+        (_, res) = foldl (\(px, res) x -> (px+x, px+x : res)) (0, []) xs
+
+    indices       = [1..n]
+    terms         = map (ugen g) indices
+    allLengths    = aggregate . map length
+    rawCounts     = allLengths terms
+    typedCounts   = allLengths $ map (filter (welltyped g)) terms
+    typedAtCounts = allLengths $ map (filter (welltypedAt g t)) terms
+    normalCounts  = allLengths $ map (filter normal . filter (welltypedAt g t)) terms
 
 printData :: Ctx -> Type -> Int -> IO ()
 printData g t n =
   mapM_ putStrLn lines
   where
-    counts = map (calculateCounts g t) $ [1..n]
+    counts = calculateCounts g t n
     addDeltaIfZero :: Int -> Double
     addDeltaIfZero n = if n == 0 then 1e-6 else fromIntegral n
     lines  = [printf "%f %f %f %f %f"
